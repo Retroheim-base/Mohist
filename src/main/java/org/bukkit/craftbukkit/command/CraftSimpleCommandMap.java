23ad93016c9102bddf1e315dc809c8147b34993f
Mgazul <mgazul@163.com>
2020-02-10 21:10:46 +0800

    Some fixes and optimizations (Uranium)

--- src/main/java/org/bukkit/craftbukkit/command/CraftSimpleCommandMap.java
+++ src/main/java/org/bukkit/craftbukkit/command/CraftSimpleCommandMap.java
@@ -1,5 +1,6 @@
 package org.bukkit.craftbukkit.command;
 
+import net.minecraft.network.rcon.RConConsoleSource;
 import org.bukkit.Bukkit;
 import org.bukkit.command.RemoteConsoleCommandSender;
 import static org.bukkit.util.Java15Compat.Arrays_copyOfRange;
@@ -52,6 +53,9 @@ public class CraftSimpleCommandMap extends SimpleCommandMap {
                 if (sender instanceof ConsoleCommandSender)
                 {
                     FMLCommonHandler.instance().getMinecraftServerInstance().getCommandManager().executeCommand(this.vanillaConsoleSender, commandLine);
+                }
+                else if (sender instanceof RemoteConsoleCommandSender){
+                    FMLCommonHandler.instance().getMinecraftServerInstance().getCommandManager().executeCommand(RConConsoleSource.instance, commandLine);
                 }
                 else FMLCommonHandler.instance().getMinecraftServerInstance().getCommandManager().executeCommand(((CraftPlayer)sender).getHandle(), commandLine);
             }
