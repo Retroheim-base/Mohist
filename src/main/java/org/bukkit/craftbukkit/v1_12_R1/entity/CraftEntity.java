47369abc75de144c32fa60f3db1f0a99fa6224c0
Mgazul <mgazul@163.com>
2020-03-06 01:17:18 +0800

    Modify error handling for unknown entity

--- src/main/java/org/bukkit/craftbukkit/v1_12_R1/entity/CraftEntity.java
+++ src/main/java/org/bukkit/craftbukkit/v1_12_R1/entity/CraftEntity.java
@@ -447,14 +447,11 @@ public abstract class CraftEntity implements org.bukkit.entity.Entity {
         } else if (entity instanceof EntityLlamaSpit) {
             return new CraftLlamaSpit(server, (EntityLlamaSpit) entity);
         }
-        // Cauldron - used for custom entities that extend Entity directly
         else if (entity instanceof net.minecraft.entity.IProjectile) {
-                return new CustomProjectileEntity(server, entity);
-        }else if (entity instanceof net.minecraft.entity.Entity) {
-            return new CraftCustomEntity(server, (net.minecraft.entity.Entity) entity);
+            return new CustomProjectileEntity(server, entity);
         }
-        return null;
-        //throw new AssertionError("Unknown entity " + (entity == null ? " is null" : entity.getClass() + ": " + entity));
+        else if (entity != null) { return new CraftCustomEntity(server, entity); }
+        throw new AssertionError("Unknown entity " + (entity == null ? " is null" : entity.getClass() + ": " + entity));
     }
 
     private static PermissibleBase getPermissibleBase() {
